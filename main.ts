b8703c8e083de0258170252b1f22f8a1b16c4ae5
Thomas Tattersfield <118993239+Thomas-Tatt@users.noreply.github.com>
2022-11-30 10:08:12 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Angry)
+})
+basic.forever(function () {
+	
+})
